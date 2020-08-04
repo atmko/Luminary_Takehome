4273701475a46eb09e29046d127a264015045d2e
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-08-04 10:51:44 -0400

    feat: add ClientAdapter constructor to PresentationModule

--- app/src/main/java/cvdevelopers/takehome/dagger/presentation/PresentationModule.kt
+++ app/src/main/java/cvdevelopers/takehome/dagger/presentation/PresentationModule.kt
@@ -1,8 +1,15 @@
 package cvdevelopers.takehome.dagger.presentation
 
+import cvdevelopers.takehome.utils.image.ImageLoader
+import cvdevelopers.takehome.view.clientlist.ClientAdapter
 import dagger.Module
+import dagger.Provides
 
 @Module
 class PresentationModule {
 
+    @Provides
+    fun provideClientAdapter(imageLoader: ImageLoader): ClientAdapter {
+        return ClientAdapter(imageLoader)
+    }
 }
--- app/src/main/java/cvdevelopers/takehome/view/clientlist/ClientListFragment.kt
+++ app/src/main/java/cvdevelopers/takehome/view/clientlist/ClientListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import cvdevelopers.githubstalker.databinding.FragmentClientListBinding
 import cvdevelopers.takehome.view.common.BaseFragment
+import javax.inject.Inject
 
 /**
  * Fragment to display client list
@@ -14,6 +15,9 @@ class ClientListFragment : BaseFragment() {
 
     private var _binding: FragmentClientListBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var clientAdapter: ClientAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
