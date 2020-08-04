98b4c44bb6d3e14648b6edb441feb615beba31ea
Aayaturobbi Mimiko <aayatmimiko@gmail.com>
2020-08-04 12:07:11 -0400

    feat: create clientListViewModel constructor
    create clientListViewModel constructor in dagger

--- app/src/main/java/cvdevelopers/takehome/dagger/presentation/ViewModelModule.java
+++ app/src/main/java/cvdevelopers/takehome/dagger/presentation/ViewModelModule.java
@@ -2,20 +2,49 @@ package cvdevelopers.takehome.dagger.presentation;
 
 import androidx.lifecycle.ViewModel;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Map;
 
 import javax.inject.Provider;
 
+import cvdevelopers.takehome.utils.executors.AppExecutors;
+import cvdevelopers.takehome.api.RandomUserApiEndpoint;
+import cvdevelopers.takehome.models.database.clientcache.ClientCacheDao;
+import cvdevelopers.takehome.viewmodel.ClientListViewModel;
 import cvdevelopers.takehome.viewmodel.ViewModelFactory;
+import dagger.MapKey;
 import dagger.Module;
 import dagger.Provides;
+import dagger.multibindings.IntoMap;
 
 @Module
 public class ViewModelModule {
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @MapKey
+    @interface ViewModelKey {
+        Class<? extends ViewModel> value();
+    }
 
     @Provides
     ViewModelFactory provideViewModelFactory
             (Map<Class<? extends ViewModel>, Provider<ViewModel>> providerMap) {
         return new ViewModelFactory(providerMap);
+    }
+
+    @Provides
+    @IntoMap
+    @ViewModelKey(ClientListViewModel.class)
+    ViewModel provideClientListViewModel(ClientCacheDao clientCacheDao,
+                                         AppExecutors appExecutors,
+                                         RandomUserApiEndpoint randomUserApiEndpoint) {
+        return new ClientListViewModel(
+                clientCacheDao,
+                appExecutors,
+                randomUserApiEndpoint);
     }
 }
